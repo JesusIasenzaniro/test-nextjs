2e53cc39a69741756cd794caec5d5f0c22e25996
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-03-07 19:54:19 +0100

    create header

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='py-3 px-2 border-solid border-2 border-b-zinc-200'>
+            <p className='font-bold text-sky-500'>Podcaster</p>
+        </header>
+    );
+};
+
+export default Header;
