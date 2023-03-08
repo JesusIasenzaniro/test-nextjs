fb7dcf3dca4c4604b8ef054980490694d2f0616c
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-03-08 19:10:36 +0100

    create header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
     return (
         <header className='py-3 px-2 border-solid border-2 border-b-zinc-200'>
-            <p className='font-bold text-sky-500'>Podcaster</p>
+            <Link href='/'>
+                <p className='font-bold text-sky-500'>Podcaster</p>
+            </Link>
         </header>
     );
 };
