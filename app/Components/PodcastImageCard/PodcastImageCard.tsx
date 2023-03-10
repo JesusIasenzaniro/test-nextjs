b51eaa5adbe03fe21104a6e3940300e8e0649be7
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-03-10 15:11:30 +0100

    refactor to PostImageCard

--- app/Components/PodcastImageCard/PodcastImageCard.tsx
+++ app/Components/PodcastImageCard/PodcastImageCard.tsx
@@ -8,21 +8,19 @@ function PodcastCard({ data }) {
             <article className='w-56 border-solid border-2 border-slate-50 shadow-md'>
                 <article className='py-4 px-8'>
                     <img
-                        src={data?.results[0]?.artworkUrl600}
-                        alt={data?.results[0]?.collectionName}
+                        src={data?.['im:image'][2]?.label}
+                        alt={data?.['im:name']?.label}
                     />
                 </article>
                 <article className='px-2'>
                     <hr />
                 </article>
                 <article className='px-2 mt-2'>
-                    <b className='podcast'>
-                        {data?.results[0]?.collectionName}
-                    </b>
+                    <b className='podcast'>{data?.['im:name']?.label}</b>
                 </article>
                 <article className='px-2 mb-2'>
                     <i className='author'>
-                        By: <span>{data?.results[0]?.collectionName}</span>
+                        By: <span>{data?.['im:artist']?.label}</span>
                     </i>
                 </article>
 
@@ -32,9 +30,7 @@ function PodcastCard({ data }) {
                 <article className='px-2 mt-2 mb-4'>
                     <b className='description-title'>Description: </b>
                     <p className='mt-2'>
-                        <i className='description'>
-                            {data?.results[1]?.shortDescription}
-                        </i>
+                        <i className='description'>{data?.summary?.label}</i>
                     </p>
                 </article>
             </article>
