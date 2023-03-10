192665757992135627bf698d8e7002351f17a7b6
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-03-10 20:50:19 +0100

    add debounce to the search input & css refactor

--- app/Components/SearchBar/SearchBar.tsx
+++ app/Components/SearchBar/SearchBar.tsx
@@ -1,23 +1,36 @@
 'use client';
-import React from 'react';
+import { debounce } from 'lodash';
+import React, { useState, useEffect } from 'react';
 import { useGlobalContext } from '../../Context/Context';
 
 import './SearchBar.css';
 
 function SearchBar() {
-    const { filter, setFilter, count } = useGlobalContext();
+    const { setFilter, count } = useGlobalContext();
+    const [inputText, setInputText] = useState('');
+
+    useEffect(() => {
+        const filterDebounce = debounce(() => {
+            setFilter(inputText);
+        }, 500);
+
+        filterDebounce();
+        return () => {
+            filterDebounce.cancel();
+        };
+    }, [inputText]);
 
     return (
-        <section className='flex justify-end items-end mt-8 mb-8'>
+        <section className='flex justify-end items-end mt-8 mb-8 '>
             <article className='bg-sky-500 px-2 py-1 rounded-lg mr-2 mb-1'>
                 <p className='text-white font-bold'>{count}</p>
             </article>
-            <form className='search-container'>
+            <form className='search-container mr-8'>
                 <input
                     type='text'
                     placeholder='Filter podcast...'
-                    onChange={(e) => setFilter(e.target.value)}
-                    value={filter}
+                    onChange={(e) => setInputText(e.target.value)}
+                    value={inputText}
                 />
             </form>
         </section>
